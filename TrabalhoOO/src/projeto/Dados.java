9edd464af8ecb6a03f9a736f37df7c5e862930d1
AntonioIgorCarvalho <78360676+AntonioIgorCarvalho@users.noreply.github.com>
2021-02-12 22:40:26 -0300

    Update Dados.java

--- TrabalhoOO/src/projeto/Dados.java
+++ TrabalhoOO/src/projeto/Dados.java
@@ -3,12 +3,12 @@ package projeto;
 public class Dados {
 	
 	
-	int dias[] = new int[13]; 
-	int med[] = new int[13];
-	int min[] = new int[13];
-	int max[] = new int[13];
-	int temperaturas[][] = new int[13][32];
-	int cadastrado[] = new int[13]; 
+	int dias[] = new int[13]; //Aqui contem o total de dias do mes -> Ex: janeiro tem 31 dias, então dias[1] == 31
+	double med[] = new double[13]; //Aqui contem a temperatura media do mes
+	double min[] = new double[13]; //Temperatura minima
+	double max[] = new double[13]; //Temperatura maxima
+	double temperaturas[][] = new double[13][32]; //Temperatura de cada dia do mes : mes/dia
+	int cadastrado[] = new int[13];  //Para checar se já foi cadastrado
 	
 	
 }
